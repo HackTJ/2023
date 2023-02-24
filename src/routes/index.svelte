24289bfc460ec43112c1ab6045c787d954ba605d
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2023-02-24 10:25:41 -0500

    Make changes to index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -41,17 +41,6 @@
 </svelte:head>
 
 <Navbar {navItems} />
-<a
-	id="mlh-trust-badge"
-	style="display:block;max-width:100px;min-width:60px;position:fixed;right:50px;top:0;width:10%;z-index:10000"
-	href="https://mlh.io/seasons/2022/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2022-season&utm_content=white"
-	target="_blank"
-	><img
-		src="https://s3.amazonaws.com/logged-assets/trust-badge/2022/mlh-trust-badge-2022-white.svg"
-		alt="Major League Hacking 2022 Hackathon Season"
-		style="width:100%"
-	/></a
->
 <!-- Home screen -->
 <div class="h-[calc(100vh_-_8rem)] min-h-[512px]">
 	<div
