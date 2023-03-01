a9a5c0e048809cfb3399d5a3c8832e738dc0bc86
Krishnan Shankar <krishnans2006@gmail.com>
2023-02-28 22:21:28 -0500

    Format changes

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -3,7 +3,7 @@
 	import LogoInstagram from '@svicons/ionicons-solid/logo-instagram.svelte';
 	import LogoLinkedIn from '@svicons/ionicons-solid/logo-linkedin.svelte';
 	import LogoTwitter from '@svicons/ionicons-solid/logo-twitter.svelte';
-	import LogoGithub from "@svicons/ionicons-solid/logo-github.svelte";
+	import LogoGithub from '@svicons/ionicons-solid/logo-github.svelte';
 
 	let classNames = '';
 	export { classNames as class };
@@ -16,16 +16,23 @@
 		<a href="https://twitter.com/hacktjofficial" target="_blank" class="hover:text-blackberry-500"
 			><LogoTwitter width="30px" /></a
 		>
-		<a href="https://www.linkedin.com/company/hacktj" target="_blank" class="hover:text-blackberry-500"
-			><LogoLinkedIn width="30px" /></a
+		<a
+			href="https://www.linkedin.com/company/hacktj"
+			target="_blank"
+			class="hover:text-blackberry-500"><LogoLinkedIn width="30px" /></a
 		>
-		<a href="https://www.instagram.com/hacktjofficial" target="_blank" class="hover:text-blackberry-500"
-			><LogoInstagram width="30px" /></a
+		<a
+			href="https://www.instagram.com/hacktjofficial"
+			target="_blank"
+			class="hover:text-blackberry-500"><LogoInstagram width="30px" /></a
 		>
-		<a href="https://www.facebook.com/HackTJOfficial/" target="_blank" class="hover:text-blackberry-500"
-			><LogoFacebook width="30px" /></a
+		<a
+			href="https://www.facebook.com/HackTJOfficial/"
+			target="_blank"
+			class="hover:text-blackberry-500"><LogoFacebook width="30px" /></a
 		>
 		<a href="https://github.com/HackTJ" target="_blank" class="hover:text-blackberry-500"
-			><LogoGithub width="30px" /></a>
+			><LogoGithub width="30px" /></a
+		>
 	</div>
 </div>
