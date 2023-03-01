ad555dcf66bfb127eacbb0536e60eddaa7f41744
Krishnan Shankar <krishnans2006@gmail.com>
2023-02-28 22:20:55 -0500

    Add GitHub link, make links open in new tab

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -3,6 +3,7 @@
 	import LogoInstagram from '@svicons/ionicons-solid/logo-instagram.svelte';
 	import LogoLinkedIn from '@svicons/ionicons-solid/logo-linkedin.svelte';
 	import LogoTwitter from '@svicons/ionicons-solid/logo-twitter.svelte';
+	import LogoGithub from "@svicons/ionicons-solid/logo-github.svelte";
 
 	let classNames = '';
 	export { classNames as class };
@@ -12,17 +13,19 @@
 	class="py-8 px-8 md:px-12 w-full bg-theme-100 flex justify-end items-center text-blackberry-800 ${classNames}"
 >
 	<div class="ml-auto flex space-x-8">
-		<a href="https://twitter.com/hacktjofficial" class="hover:text-blackberry-500"
+		<a href="https://twitter.com/hacktjofficial" target="_blank" class="hover:text-blackberry-500"
 			><LogoTwitter width="30px" /></a
 		>
-		<a href="https://www.linkedin.com/company/hacktj" class="hover:text-blackberry-500"
+		<a href="https://www.linkedin.com/company/hacktj" target="_blank" class="hover:text-blackberry-500"
 			><LogoLinkedIn width="30px" /></a
 		>
-		<a href="https://www.instagram.com/hacktjofficial" class="hover:text-blackberry-500"
+		<a href="https://www.instagram.com/hacktjofficial" target="_blank" class="hover:text-blackberry-500"
 			><LogoInstagram width="30px" /></a
 		>
-		<a href="https://www.facebook.com/HackTJOfficial/" class="hover:text-blackberry-500"
+		<a href="https://www.facebook.com/HackTJOfficial/" target="_blank" class="hover:text-blackberry-500"
 			><LogoFacebook width="30px" /></a
 		>
+		<a href="https://github.com/HackTJ" target="_blank" class="hover:text-blackberry-500"
+			><LogoGithub width="30px" /></a>
 	</div>
 </div>
